fda2c3832496a90f4e4ccdc9239877d41b07e9c3
Jesus Glez <jesusgleztr94@gmail.com>
2023-09-25 19:05:09 -0600

    Feature: Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,72 @@
+import Container from './container/Container'
+import { TfiLocationPin } from 'react-icons/tfi'
+import { FiPhone, FiFacebook, FiInstagram, FiMail } from 'react-icons/fi'
+
 export default function Footer() {
-	return <footer></footer>
+	return (
+		<footer className='bg-dorado'>
+			<Container className='text-black text-center py-[25px] text-sm md:text-sm'>
+				<div className='grid gap-4 sm:grid-cols-2 lg:grid-cols-4 md:items-center'>
+					<p>
+						<TfiLocationPin
+							className='inline mr-1'
+							size={16}
+						/>
+						<span>Calle 27 #218, entre 26 y 28, García Ginerés.</span>
+					</p>
+					<p>
+						<a href='tel:+5512273282'>
+							<FiPhone
+								className='inline mr-1'
+								size={16}
+							/>
+							<span>(55)1 227 3282</span>
+						</a>
+					</p>
+					<p>
+						<a
+							href='http://'
+							target='_blank'
+							rel='noopener noreferrer'
+							className='mr-2'>
+							<FiFacebook
+								className='inline'
+								size={16}
+							/>
+						</a>
+						<a
+							href='http://'
+							target='_blank'
+							rel='noopener noreferrer'>
+							<FiInstagram
+								className='inline mr-1'
+								size={16}
+							/>{' '}
+							<span className='underline'>casaencuentro_mx/</span>
+						</a>
+					</p>
+					<p>
+						<a href='mailto:casaencuentrog@gmail.com'>
+							<FiMail
+								className='inline mr-1'
+								size={16}
+							/>
+							casaencuentrog@gmail.com
+						</a>
+					</p>
+				</div>
+			</Container>
+
+			<div className='bg-black text-sm md:text-base text-center py-[15px]'>
+				<p>
+					<a
+						href='http://'
+						target='_blank'
+						rel='noopener noreferrer'>
+						Política de privacidad
+					</a>
+				</p>
+			</div>
+		</footer>
+	)
 }
