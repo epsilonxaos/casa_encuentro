1e70ffd42e05301ea10dc3dfdca2605538ad2b24
Jesus Glez <angelotr12@hotmail.com>
2023-10-09 17:22:36 -0600

    Hotfix: Menu movil animacion

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,32 +16,51 @@ import { AnimatePresence, motion } from 'framer-motion'
 
 export default function Header() {
 	const [open, setOpen] = useState(false)
+	const v = {
+		hide: {
+			top: -10,
+		},
+		show: {
+			top: 80,
+		},
+	}
 
 	return (
 		<>
-			<header className='fixed top-0 left-0 w-full z-50 bg-black text-xs md:text-base'>
-				{/* //TODO: Verificar los espaciados de padding en X */}
-				<Container className=''>
-					<nav className='flex items-center justify-between py-[25px]'>
-						<div className='md:hidden'>
-							<Language />
-						</div>
-						<img
-							src={logo}
-							alt='Casa encuentro'
-							className='w-[180px]'
-						/>
-						<Menu className='hidden md:flex md:items-center md:gap-[33px] menuText' />
-						<button
-							onClick={() => setOpen(!open)}
-							className='border border-dorado px-2 py-1 md:hidden'>
-							Menú
-						</button>
-					</nav>
-				</Container>
-				{open && (
-					<Menu className='w-full fixed top-[80px] left-0 py-[25px] bg-black md:hidden flex items-center justify-center gap-[20px] menuText' />
-				)}
+			<header className='fixed top-0 left-0 w-full z-50 text-xs md:text-base'>
+				<div className='bg-black'>
+					{/* //TODO: Verificar los espaciados de padding en X */}
+					<Container className=''>
+						<nav className='flex items-center justify-between py-[25px]'>
+							<div className='md:hidden'>
+								<Language />
+							</div>
+							<img
+								src={logo}
+								alt='Casa encuentro'
+								className='w-[180px]'
+							/>
+							<Menu className='hidden md:flex md:items-center md:gap-[33px] menuText' />
+							<button
+								onClick={() => setOpen(!open)}
+								className='border border-dorado px-2 py-1 md:hidden'>
+								Menú
+							</button>
+						</nav>
+					</Container>
+				</div>
+				<AnimatePresence mode='wait'>
+					{open && (
+						<motion.div
+							initial={v.hide}
+							animate={v.show}
+							exit={v.hide}
+							transition={{ duration: 0.3 }}
+							className='w-full fixed z-[-1] top-[80px] left-0 py-[25px] pt-5 bg-black md:hidden'>
+							<Menu className='flex items-center justify-center gap-[20px] menuText pt-0' />
+						</motion.div>
+					)}
+				</AnimatePresence>
 			</header>
 
 			<FormReservaciones />
@@ -128,7 +147,7 @@ const FormReservaciones = () => {
 					initial={v.close}
 					animate={open ? v.open : v.close}
 					exit={open ? v.open : v.close}
-					transition={{ duration: 0.5 }}
+					transition={{ duration: 0.3 }}
 					className='relative px-2 bg-black rounded-b z-[1] overflow-hidden'>
 					<form
 						action='https://rbe.zaviaerp.com/'
