7658c76de53afe007d8e9e723aa5fe499123705e
Jesus Glez <jesus.gonzalez@merida.gob.mx>
2023-10-09 11:05:02 -0600

    Hotfix: amenidades movil

--- src/components/sections/Amenidades.jsx
+++ src/components/sections/Amenidades.jsx
@@ -13,18 +13,19 @@ export default function Amenidades() {
 			<Container className='px-[50px] md:px-[80px] lg:px-[120px] pt-[90px] overflow-hidden relative max-w-[1320px]'>
 				<Titulo.H2 className='max-w-[345px]  2xl:max-w-[430px] mx-auto'>{t('home.amenidades.titulo')}</Titulo.H2>
 				<hr className='border-crema w-full max-w-[215px] mx-auto mb-[40px]' />
-
+			</Container>
+			<Container className='px-0 overflow-hidden relative max-w-[1320px]'>
 				<div className='flex flex-wrap justify-center'>
 					{amenidades.map((k, key) => (
 						<div
-							className='text-center w-1/2 sm:w-1/3 lg:w-1/4 xl:w-1/6 mb-[20px]'
+							className='text-center w-1/4 sm:w-1/3 lg:w-1/4 xl:w-1/6 mb-[20px]'
 							key={'amenidad-' + key}>
 							<img
 								src={k.img}
 								alt='Amenidad'
-								className='max-h-[45px] max-w-[64px] mb-2 inline-block'
+								className='max-h-[25px] max-w-[64px] mb-2 inline-block'
 							/>
-							<Parrafo>{k.title[i18n.language]}</Parrafo>
+							<Parrafo className='text-xs'>{k.title[i18n.language]}</Parrafo>
 						</div>
 					))}
 				</div>
