9168ad657acdb9aee0353cf626e9fb97c3a86587
Jesus Glez <angelotr12@hotmail.com>
2023-10-10 19:04:10 -0600

    Url mapa

--- src/components/sections/Ubicacion.jsx
+++ src/components/sections/Ubicacion.jsx
@@ -12,17 +12,21 @@ export default function Ubicacion() {
 		<section className='pt-[90px]'>
 			<Titulo.H2 className='mb-0'>{t('home.ubicacion.titulo')}</Titulo.H2>
 			<Parrafo className='text-center'>{t('home.ubicacion.direccion')}</Parrafo>
-
-			<img
-				className='w-full max-w-max hidden md:block'
-				src={mapa}
-				alt='Mapa'
-			/>
-			<img
-				className='w-full max-w-max md:hidden'
-				src={mapaMovil}
-				alt='Mapa movil'
-			/>
+			<a
+				href='https://maps.app.goo.gl/VtxcQJ2btYAxmKy27'
+				target='_blank'
+				rel='noopener noreferrer'>
+				<img
+					className='w-full max-w-max hidden md:block'
+					src={mapa}
+					alt='Mapa'
+				/>
+				<img
+					className='w-full max-w-max md:hidden'
+					src={mapaMovil}
+					alt='Mapa movil'
+				/>
+			</a>
 		</section>
 	)
 }
